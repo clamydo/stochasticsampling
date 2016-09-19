c4ea4287a41d24ec95f5a90d4baf996c1d81d8af
fkjogu <fkjogu@users.noreply.github.com>
2016-09-19 20:33:43 +0200

    Implement simpsons rule and prepare calculateion of stress tensor.

--- src/simulation/integrator.rs
+++ src/simulation/integrator.rs
@@ -1,6 +1,6 @@
 use coordinates::TWOPI;
 use coordinates::particle::Particle;
-use ndarray::{Array, Ix};
+use ndarray::{Array, Axis, Ix};
 use settings::{DiffusionConstants, GridSize, StressPrefactors};
 use super::DiffusionParameter;
 use super::distribution::GridWidth;
@@ -39,7 +39,7 @@ impl Integrator {
     /// Calculates force on the flow field because of the sress contributions.
     /// The first axis represents the direction of the derivative, the other
     /// correspond to the spatial dimension.
-    /// 
+    ///
     /// self.stress is a 2 x 2 matrix sampled for different angles,
     /// resulting in 2 x 2 x a
     /// dist is a 2 x l x t x a, with l x t spatial samples
@@ -48,14 +48,14 @@ impl Integrator {
     ///
     /// In Python's numpy, I'd do
     /// ´´´
-    /// t[0, nx, ny, :] = s[:, 0, 0] * d[0, nx, ny, :] + s[:, 1, 0] * d[1, nx, ny, :] 
-    /// t[1, nx, ny, :] = s[:, 0, 1] * d[0, nx, ny, :] + s[:, 1, 1] * d[1, nx, ny, :] 
+    /// t[0, nx, ny, :] = s[:, 0, 0] * d[0, nx, ny, :] + s[:, 1, 0] * d[1, nx, ny, :]
+    /// t[1, nx, ny, :] = s[:, 0, 1] * d[0, nx, ny, :] + s[:, 1, 1] * d[1, nx, ny, :]
     /// ´´´
     ///
-    /// Followed by an simpson rule integration along the angular axis for 
-    /// every component of the flow field u and for every point in 
+    /// Followed by an simpson rule integration along the angular axis for
+    /// every component of the flow field u and for every point in
     /// space (nx, ny).
-    /// 
+    ///
     /// Example suggestive implementation in Python for first component of u
     /// and position (nx, ny).
     /// ´´´
@@ -67,32 +67,61 @@ impl Integrator {
     /// for i in range(2, na - 1, 2):
     ///     l += 2 * t[0, nx, ny, i]
     ///
-    /// f[0, nx, ny] = l * grid_width_angle / 3 
+    /// f[0, nx, ny] = l * grid_width_angle / 3
     /// ´´´
     fn calc_stress_gradient(&self, dist: &Distribution) -> Array<f64, (Ix, Ix, Ix)> {
         let h = dist.get_grid_width();
 
-        let shape = dist.shape();
-
-        let mut res = Array::zeros((shape.0, shape.1, 2));
-
         // Calculate just first component
 
-        // Calculates (grad Psi)_i * stress_kernel_(i, j) for every point on the
-        // grid and j = 0.
-        // The subview returns only the first colum of every submatrix of the
-        // stress kernel. This makes use of broadcasting the stress kernel to
-        // every point on the grid. Finally the last axis ist contracted.
+        // Calculates (grad Psi)_i * stress_kernel_(i, j) for every point on the grid and j = 0.
+        // This makes implicit and explicit use of broadcasting. Implicetly the stress kernel ´sk´
+        // is broadcasted for all points in space. Then, explicetly, the gradient ´g´ is
+        // broadcasted along the last axis. This effectively repeats the gradient along the second
+        // index of the stress kernel ´sk´. Multiplying it elementwise results in
         //
-        // Two calculate both components at the same time, maybe
-        // `dist.spatgrad()` could be broadcasted along the last axis? So
-        // basically adding an axis of size one at the end and broadcast to 2. 
-        let x_comp = (dist.spatgrad() * self.stress_kernel.subview(Axis(2), 0)).sum(Axis(3)); 
-        let y_comp = (dist.spatgrad() * self.stress_kernel.subview(Axis(2), 0)).sum(Axis(3)); 
+        //  [[g_1, g_1],  *  [[sk_11, sk_12],  =  [[g_1 * sk_11, g_1 * sk_12],
+        //   [g_2, g_2]]      [sk_21, sk_22]]      [g_2 * sk_21, g_2 * sk_22]
+        //
+        // Now buy summing along the first index of the matrix, it results in
+        // [ g_1 * sk_11 + g_2 * sk_21, g_1 * sk_12 + g_2 * sk_22]
+        //
+        // This is done for every point (x, y, alpha).
 
+        let g = dist.spatgrad();
+        let ref sk = self.stress_kernel;
+
+        let sh = g.dim();
+        let sh_a = (sh.0, sh.1, sh.2, sh.3, 1);
+        let sh_b = (sh.0, sh.1, sh.2, sh.3, 2);
+
+        let fun = (g.into_shape(sh).unwrap().broadcast(sh_b).unwrap().to_owned() * sk).sum(Axis(3));
+
+        unimplemented!()
     }
 }
 
+/// Implements Simpon's Rule integration on an array, representing sampled points of a periodic
+/// function.
+fn periodic_simpson_integrate(samples: &Array<f64, (Ix)>, h: f64) -> f64 {
+    let len = samples.dim();
+
+    assert!(len % 2 == 0,
+            "Periodic Simpson's rule only works for even number of sample points, since the \
+             first point in the integration interval is also the last.");
+
+    unsafe {
+        let mut s = samples.uget(0) + samples.uget(0);
+
+        for i in 1..(len / 2) {
+            s += 2. * samples.uget(2 * i);
+            s += 4. * samples.uget(2 * i - 1);
+        }
+
+        s += 4. * samples.uget(len - 1);
+        s * h / 3.
+    }
+}
 
 pub fn evolve_inplace<F>(p: &mut Particle, diffusion: &DiffusionConstants, timestep: f64, mut c: F)
     where F: FnMut() -> f64
@@ -111,9 +140,12 @@ pub fn evolve_inplace<F>(p: &mut Particle, diffusion: &DiffusionConstants, times
 mod tests {
     use coordinates::TWOPI;
     use coordinates::particle::Particle;
+    use ndarray::Array;
     use settings::{DiffusionConstants, GridSize, StressPrefactors};
     use super::*;
     use super::super::distribution::GridWidth;
+    use std::f64::consts::PI;
+    use std::f64::EPSILON;
 
     #[test]
     fn new() {
@@ -149,5 +181,26 @@ mod tests {
         assert_eq!(p.position.x.v, 0.5);
         assert_eq!(p.position.y.v, 0.6);
         assert_eq!(p.orientation.v, 1.2);
+    }
+
+    #[test]
+    fn test_simpson() {
+        let h = PI / 100.;
+        let f = Array::range(0., PI, h).map(|x| x.sin());
+        let integral = super::periodic_simpson_integrate(&f, h);
+
+        assert!((integral - 2.000000010824505).abs() < EPSILON,
+                "h: {}, result: {}",
+                h,
+                integral);
+
+
+        let h = 4. / 100.;
+        let f = Array::range(0., 4., h).map(|x| x * x);
+        let integral = super::periodic_simpson_integrate(&f, h);
+        assert!((integral - 21.120000000000001).abs() < EPSILON,
+                "h: {}, result: {}",
+                h,
+                integral);
     }
 }
