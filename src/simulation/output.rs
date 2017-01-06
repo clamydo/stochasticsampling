c2ee29b81140a934e202ff3bfdf0521ec307cb93
fkjogu <fkjogu@users.noreply.github.com>
2017-01-06 13:50:44 +0100

    Formatting.

--- src/simulation/output.rs
+++ src/simulation/output.rs
@@ -1,7 +1,6 @@
-use super::integrator::FlowField;
 use super::distribution::Distribution;
+use super::integrator::FlowField;
 use super::super::coordinates::particle::Particle;
-
 
 /// Captures values that can be outputed during simulation.
 /// Not all fields need to have values, which is reflected in the Option type.
