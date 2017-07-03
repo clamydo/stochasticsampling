038fd20dcad40747ac605e272c71b068516d2239
fkjogu <fkjogu@users.noreply.github.com>
2017-07-03 14:12:04 +0200

    Typo

--- build.rs
+++ build.rs
@@ -6,6 +6,6 @@ fn main() {
     vergen(SHORT_SHA).unwrap();
     println!("rustc-flags=-C target-cpu=native");
     // println!("cargo:rustc-link-search=native=/path/to/lib");
-    pintln!("cargo:rustc-link-lib=static=fftw3");
+    println!("cargo:rustc-link-lib=static=fftw3");
 
 }
