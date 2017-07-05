6d5be677f4b11da801da10bd40c499457eca3a4b
fkjogu <fkjogu@users.noreply.github.com>
2017-07-05 10:23:42 +0200

    Increment version

--- build.rs
+++ build.rs
@@ -7,5 +7,4 @@ fn main() {
     println!("rustc-flags=-C target-cpu=native");
     println!("cargo:rustc-link-search=native=/usr/lib/x86_64-linux-gnu");
     println!("cargo:rustc-link-lib=static=fftw3");
-
 }
