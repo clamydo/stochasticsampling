f16fc23749a4c7d637758294e363a08d0e611384
fkjogu <fkjogu@users.noreply.github.com>
2017-10-10 17:04:33 +0200

    Fix again bug with time display

--- src/bin/timedisplay.rs
+++ src/bin/timedisplay.rs
@@ -3,8 +3,8 @@ use ::time::Duration;
 pub fn pretty_print_duration(d: Duration) -> String {
     let days = d.num_days();
     let hours = d.num_hours() - days * 24;
-    let minutes = d.num_minutes() - days * 24 * 60 - hours * 60 * 60;
-    let seconds = d.num_seconds() - days * 24 * 60 - hours * 60 * 60 - minutes * 60;
+    let minutes = d.num_minutes() - days * 24 * 60 - hours * 60;
+    let seconds = d.num_seconds() - days * 24 * 60 * 60 - hours * 60 * 60 - minutes * 60;
 
     format!(
         "{d} days {h} hours {m} minutes {s} seconds",
