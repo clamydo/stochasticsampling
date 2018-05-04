a269b43b8e9a1ebfc506c8e13d89a2d03c625909
fkjogu <fkjogu@users.noreply.github.com>
2018-05-04 17:34:55 +0200

    Add test for polarization calculation

--- src/simulation/polarization/director_test.rs
+++ src/simulation/polarization/director_test.rs
@@ -1,6 +1,7 @@
 use super::*;
-use simulation::settings::{BoxSize, GridSize};
 use simulation::particle::Particle;
+use simulation::settings::{BoxSize, GridSize};
+use test_helper::equal_floats;
 
 #[test]
 fn test_polarization_from_distribution() {
@@ -19,25 +20,42 @@ fn test_polarization_from_distribution() {
 
     let gw = GridWidth::new(gs, bs);
 
-    let p = Particle::new(0.0, 0.0, 0.0, PI, PI / 2., bs);
+    let p1 = Particle::new(0.0, 0.0, 0.0, PI, PI / 2., bs);
+    let p2 = Particle::new(1.5, 2.5, 0.0, 0.269279370307697, 0.942477796076938, bs);
     let mut d = Distribution::new(gs, bs);
-    let p = vec![p];
+    let p = vec![p1, p2];
+    let n = p.len() as f64;
     d.sample_from(&p);
 
     let mut p = DirectorField::new(gs, gw);
 
     p.from_distribution(&d);
 
-    // assert!(false, "{:?}", p.field)
-    assert_eq!(p.field[[0, 0, 0, 0]].re, -1.0_f64);
-    assert_eq!((p.field[[1, 0, 0, 0]].re * 10e14).round(), 0.0_f64);
-    assert_eq!((p.field[[2, 0, 0, 0]].re * 10e14).round(), 0.0_f64);
+    assert_eq!(p.field[[0, 0, 0, 0]].re, -1.0_f64 / n);
+    assert_eq!((p.field[[1, 0, 0, 0]].re * 10e14).round(), 0.0_f64 / n);
+    assert_eq!((p.field[[2, 0, 0, 0]].re * 10e14).round(), 0.0_f64 / n);
 
-
-    p.field.slice_mut(s![.., 0, 0, 0])
+    // set singular entry to zero
+    p.field
+        .slice_mut(s![.., 0, 0, 0])
         .map_inplace(|v| *v = 0.0_f64.into());
 
+    assert!(equal_floats(p.field[[0, 1, 2, 0]].re, 0.7798623362492354 / n));
+    assert!(equal_floats(p.field[[1, 1, 2, 0]].re, 0.2152283291933436 / n));
+    assert!(equal_floats(p.field[[2, 1, 2, 0]].re, 0.5877852522924731 / n));
+
+    // set singular entry to zero
+    p.field
+        .slice_mut(s![.., 1, 2, 0])
+        .map_inplace(|v| *v = 0.0_f64.into());
+
+    // check the rest
     for (i, v) in p.field.iter().enumerate() {
-        assert!(*v == 0.0f64.into(), "Value at index {} should be zero, but is {}.", i, v);
+        assert!(
+            *v == 0.0f64.into(),
+            "Value at index {} should be zero, but is {}.",
+            i,
+            v
+        );
     }
 }
