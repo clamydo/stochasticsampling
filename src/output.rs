561bb8ff348c3a7396b6b69429bd73f259b6e862
fkjogu <fkjogu@users.noreply.github.com>
2018-12-07 14:27:25 +0100

    Make OutputEntry derserializable

--- src/output.rs
+++ src/output.rs
@@ -5,7 +5,7 @@ use ndarray::{Array, Ix4};
 
 /// Captures values that can be outputed during simulation.
 /// Not all fields need to have values, which is reflected in the Option type.
-#[derive(Debug, Default, Serialize)]
+#[derive(Debug, Default, Serialize, Deserialize)]
 pub struct OutputEntry {
     pub distribution: Option<Distribution>,
     pub flowfield: Option<FlowField3D>,
